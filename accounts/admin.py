2516f0bfd19e0f24323713698fac3c40f997e9b6
Elsaid <elsaid.rexha@stud.unifi.it>
2026-05-27 14:13:19 +0200

    Initial setup: Aggiunto lo user

---
+++ accounts/admin.py
@@ -0,0 +1,12 @@
+# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        ('Ruolo', {'fields': ('role',)}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
---
+++ accounts/models.py
@@ -0,0 +1,14 @@
+# Create your models here.
+# accounts/models.py
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = [
+        ('user', 'Standard User'),
+        ('coach', 'Coach'),
+    ]
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='user')
+
+    def is_coach(self):
+        return self.role == 'coach'
